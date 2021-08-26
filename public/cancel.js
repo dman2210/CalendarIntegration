5a4fb2fca4ef80bcf5798bc6d7d1121f984dacab
dman2210 <32994314+dman2210@users.noreply.github.com>
2021-08-26 14:09:44 -0600

    debugging cancel

--- public/cancel.js
+++ public/cancel.js
@@ -60,9 +60,12 @@ async function cancelAll() {
     document.getElementById('prompt').style.display = 'none';
     let url = "https://calendar-integration-backend.vercel.app/api/appointments?action=cancel";
     // let url = "http://localhost:3000/api/appointments?action=cancel";
-    let customerID = queryParams.get('customerID'); await skipNextInvoice(body.customerID);
+    let customerID = queryParams.get('customerID');
     let eventID = "all"
     let body = { customerID: customerID, id: eventID };
+    ["where", "when", "who", "email", "eventID", "current", "customerID"].forEach((item) => {
+        body[item] = queryParams.get(item);
+    })
     console.log(body)
     // await fetch(url, { method: "POST", body: JSON.stringify(body) });
     document.getElementById('loaderContainer').style.display = 'none';
