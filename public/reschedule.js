fa04b9be18d341f365f72020a76fdffc075356f1
dman2210 <32994314+dman2210@users.noreply.github.com>
2021-10-25 10:30:46 -0600

    type fixed

--- public/reschedule.js
+++ public/reschedule.js
@@ -30,7 +30,7 @@ function goTo() {
 
 function showCurrent() {
     let currDate = new Date(queryParams.get('now'));
-    if (Math.rouMath.abs(new Date() - currDate) / 1000 / 60 / 60 < 24) {
+    if (Math.round(Math.abs(new Date() - currDate) / 1000 / 60 / 60) < 24) {
         document.getElementById("mainContainer").innerHTML = "Appointments cannot be rescheduled within 24 hours of the appointment time.";
     } else {
         let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
